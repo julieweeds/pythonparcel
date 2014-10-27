f58a24dd384ae00810b70b614e3e9e9a4ff03f9f
julieweeds <julie.weeds@gmail.com>
2014-10-27 22:48:57 +0000

    nltk generation

---
+++ src/nltkgen.py
@@ -0,0 +1,83 @@
+__author__ = 'juliewe'
+import sys
+from nltk import CFG
+from nltk.parse.generate import generate
+
+class CorpusGenerator:
+
+    no_sents=10
+    debug=False
+
+    def __init__(self):
+        self.grammarstring=""
+
+    def readfile(self,filename):
+        
+
+        print "Reading "+filename
+        processed=0
+        with open(filename) as instream:
+            for line in instream:
+                processed+=self.processline(line)
+                if processed>0 and CorpusGenerator.debug:
+                    break
+        self.grammar=CFG.fromstring(self.grammarstring)
+        self.start=self.grammar.start()
+
+    def processline(self,line):
+
+        #check not blank or comment
+        if(len(line)>0 and not line.startswith("//")):
+            self.grammarstring+=line+" "
+            return 1
+        else:
+            return 0
+
+    def showgrammar(self):
+        print self.grammar
+
+    def generate(self,topstart="top",n=no_sents):
+        if topstart=="top":
+            topstart=self.start
+        else:
+            topstart=self.findstart(topstart)
+
+        if n>0:
+            max=n
+        else:
+            max=CorpusGenerator.no_sents
+
+        sentences=0
+        for sentence in generate(self.grammar,start=topstart,n=max):
+#        for sentence in generate(self.grammar,n=self.no_sents):
+            print (' '.join(sentence))
+            sentences+=1
+        print "Produced sentences: "+str(sentences)
+
+    def findstart(self,start):
+        for prod in self.grammar.productions():
+            #print prod.lhs(),start
+            if str(prod.lhs())==start:
+                #print prod
+                return prod.lhs()
+
+
+if __name__=='__main__':
+
+    filename=sys.argv[1]
+    if len(sys.argv)>2:
+        start=sys.argv[2]
+    else:
+        start="top"
+    if len(sys.argv)>3:
+        no_sents=int(sys.argv[3])
+    else:
+        no_sents=0
+    myGen = CorpusGenerator()
+    myGen.readfile(filename)
+    print "Starting with rules for: "+start
+    #myGen.showgrammar()
+    #myGen.findrules(start)
+    myGen.generate(start,n=no_sents)
+
+
